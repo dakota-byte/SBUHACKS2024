ae25030e80415bc2d17fe9f20eda7d7fcc33c957
Akib Yeahia <akib.yeahia@stonybrook.edu>
2024-02-10 17:01:18 -0500

    different background

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -1,60 +1,43 @@
 // Home.js
 import React from 'react';
 
-import { Kbd } from '@chakra-ui/react'
+import { Center, Kbd } from '@chakra-ui/react'
 import { Card, Image, Stack, Heading, Text, Divider, ButtonGroup, Button, CardBody, CardFooter } from '@chakra-ui/react'
 
 import { Avatar, AvatarBadge } from '@chakra-ui/react'
 import Navigation from "./Navigation.jsx"
 
-const Home = () => {
-  return (
-    <div>
-      <Navigation/>
-      <Stack direction='row' spacing={4}>
-  <Avatar>
-    <AvatarBadge boxSize='1.25em' bg='green.500' />
-  </Avatar>
+import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
 
-  {/* You can also change the borderColor and bg of the badge */}
-  <Avatar>
-    <AvatarBadge borderColor='papayawhip' bg='tomato' boxSize='1.25em' />
-  </Avatar>
-</Stack>
-      <h1>Welcome to the homepage!</h1>
-      <span>
-        <Kbd>alt</Kbd> or <Kbd>option</Kbd>
-      </span>
-      <Card maxW='sm'>
-  <CardBody>
-    <Image
-      src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-      alt='Green double couch with wooden legs'
-      borderRadius='lg'
-    />
-    <Stack mt='6' spacing='3'>
-      <Heading size='md'>kyles soul</Heading>
-      <Text>
-        this sofa is kyles soul
-      </Text>
-      <Text color='blue.600' fontSize='2xl'>
-        $69
-      </Text>
-    </Stack>
-  </CardBody>
-  <Divider />
-  <CardFooter>
-    <ButtonGroup spacing='2'>
-      <Button variant='solid' colorScheme='blue'>
-        Eat now
-      </Button>
-      <Button variant='ghost' colorScheme='blue'>
-        Eat later
-      </Button>
-    </ButtonGroup>
-  </CardFooter>
-</Card>
+import './animation.css';
+
+const Home = () => {
+  <style>
+  {`
+    @import url('https://fonts.googleapis.com/css2?family=Bubblegum+Sans&display=swap');
+  `} </style>
+  
+
+  const headingStyles = defineStyle({
+    textAlign: 'center', // Center the text
+    marginTop: '3em', // Indent the text by 5 lines
+    color: '#90e0ef',
+    fontFamily: 'Bubblegum Sans, sans-serif', // Change the font to Arial
+  });
+
+  return (
+    <div className="animated-background">
+      <Navigation/>
+      <Stack direction='row' spacing={4}> </Stack>
+
+      <Stack spacing={1} align = "center">
+        <Heading as='h1' size='3xl' noOfLines={20} style={headingStyles}>
+          DoodleDash
+        </Heading>
+      </Stack>
     </div>
+
+    
   );
 };
 
