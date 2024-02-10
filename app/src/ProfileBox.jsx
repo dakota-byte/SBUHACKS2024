fe00d34baf4d073d8cf0af44b7f046a0d12dc735
KymaiseHunter <Kyle.Bautista78@myhunter.cuny.edu>
2024-02-10 15:13:05 -0500

    link is now a param to Profile box

--- app/src/ProfileBox.jsx
+++ app/src/ProfileBox.jsx
@@ -1,10 +1,12 @@
 import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+
 
 function ProfileBox(props)
 {
     return(
         <div> 
-            <Avatar name = {props.name} src='https://bit.ly/broken-link' />
+            <Avatar name = {props.name} src={props.link} />
         </div>
     );
 }export default ProfileBox
--- app/src/test.jsx
+++ app/src/test.jsx
@@ -9,9 +9,10 @@ const KyleTest = () => {
   const placeHolderName1 = "John Doe";
   const placeHolderName2 = "Kyle";
 
+  const placeHolderLink1 = "https://bit.ly/sage-adebayo"
   return (
     <div>
-      <ProfileBox name = {placeHolderName1}></ProfileBox>
+      <ProfileBox name = {placeHolderName1} link = {placeHolderLink1}></ProfileBox>
       <ProfileBox name = {placeHolderName2}></ProfileBox>
     </div>
   );
