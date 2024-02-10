3c5b9f69a49e91ca91673c560f2fef9731b019cd
KymaiseHunter <Kyle.Bautista78@myhunter.cuny.edu>
2024-02-10 15:49:01 -0500

    Profile Box finished

--- app/src/test.jsx
+++ app/src/test.jsx
@@ -8,12 +8,14 @@ import ProfileBox from "./ProfileBox.jsx"
 const KyleTest = () => {
   const placeHolderName1 = "John Doe";
   const placeHolderName2 = "Kyle";
+  const placeHolderName3 = "Kyle Robert Batman Bautista";
 
   const placeHolderLink1 = "https://bit.ly/sage-adebayo"
   return (
     <div>
       <ProfileBox name = {placeHolderName1} link = {placeHolderLink1}></ProfileBox>
-      <ProfileBox name = {placeHolderName2}></ProfileBox>
+      <ProfileBox name = {placeHolderName2} link = {'https://bit.ly/broken-link'}></ProfileBox>
+      <ProfileBox name = {placeHolderName3} link = {'https://bit.ly/broken-link'}></ProfileBox>
     </div>
   );
 };
